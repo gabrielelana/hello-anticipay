5cd664590a2bbea1f5bba4432f150b40e7038d1d
gabriele.lana <gabriele.lana@cleancode.it>
2017-07-05 15:24:30 +0200

    React with style!

--- ui/js/app.jsx
+++ ui/js/app.jsx
@@ -1,6 +1,9 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 class App extends React.Component {
   constructor(props) {
     super();
@@ -23,12 +26,14 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <button onClick={this.down.bind(this)}>Down</button>
-        <h1>{this.state.count}</h1>
-        <button onClick={this.up.bind(this)}>Up</button>
-      </div>
-    )
+      <MuiThemeProvider>
+        <div>
+          <RaisedButton onClick={this.down.bind(this)} primary={true} label="Down" />
+          <span className="display-3" style={{padding: "10px"}}>{this.state.count}</span>
+          <RaisedButton onClick={this.up.bind(this)} primary={true} label="Up" />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
